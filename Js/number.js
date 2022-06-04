635d6bce44d7d735cd65b053a33b1ddc21d768f9
marlisonmourao <bentesmourao@gmail.com>
2022-06-04 02:08:36 -0400

    first commit

---
+++ Js/number.js
@@ -0,0 +1,25 @@
+var idade = 22;
+console.log('Minha idade é: ' + idade);
+console.log('que tipo de dado é a variavel idade ? ' + typeof idade);
+
+var precoFeijao =  10.00;
+console.log(precoFeijao);
+console.log(typeof precoFeijao);
+
+var valor = 23 + 33;
+console.log(valor);
+
+var valores = '23' + 33;
+console.log(valores);
+console.log(valor.toString(10));
+console.log(valor.toString(2));
+console.log(valor.toString(8));
+console.log(valor.toString(16));
+let numero = -34;
+console.log(numero);
+
+let soma = (numero + valor);
+console.log(soma);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
---
+++ Js/string.js
@@ -0,0 +1,19 @@
+var nome = 'Marlison ';
+var sobrenome = 'Bentes Mourão';
+var nomeCompleto = nome + sobrenome;
+var vazia = 'texto \
+texto \ '
+
+console.log('nome completo: ' + nomeCompleto);
+
+var numeroString = '20' + 10;
+var num = 10;
+
+console.log('valor: ' + numeroString);
+var stringVar = 'StringTexto'
+
+console.log('tipo: ' + typeof stringVar);
+console.log('tipo1: ' + typeof num);
+
+console.log('tamanho String: ' + stringVar.length);
+console.log('tamanho String ' + nomeCompleto.length)
